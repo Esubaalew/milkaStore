7776e7b7dc92f501a1d789427238ed8cb02fadb0
esubaalew <esubalewchekol6@gmail.com>
2024-10-23 11:33:10 +0300

    translation

--- shop/serializers.py
+++ shop/serializers.py
@@ -3,9 +3,17 @@ from .models import Product, Order
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    available_stock = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_available_stock(self, obj):
+        stocks = obj.stocks.all()
+        if stocks.exists():
+            return stocks.first().quantity_in_stock
+        return 0
 
 
 class OrderSerializer(serializers.ModelSerializer):
